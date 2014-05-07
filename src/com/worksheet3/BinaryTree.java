3e703afc3672e1adff5d3710989c6cc00a5dcd0c
wongchingchai <naapat_101@hotmail.com>
2014-05-07 20:00:31 +0700

    WorkLab3[BinaryTree]

---
+++ src/com/worksheet3/BinaryTree.java
@@ -0,0 +1,180 @@
+package com.worksheet3;
+
+public class BinaryTree {
+
+	  Node root;
+	 	 
+	  	    public void addNode(int key,String name) {
+	  	
+	  	    Node NewNode = new Node(key, name);
+	  	     // Node newNode = new Node( key, name);
+	  	    if (root == null) {
+	  	    	root = NewNode;
+	  	 
+	  	        } else {
+	  	 
+	  	         Node FocusNode = root;
+	  	         Node parent;
+	  	 
+	  	          while (true) {
+	  	 
+	  	          parent = FocusNode;
+	  	
+	  	          //if (key < focusNode.key) {
+	  	          if (key < FocusNode.key) {
+	  	               FocusNode = FocusNode.leftChild;
+	  	 
+	  	          if (FocusNode == null) {
+	  	        	  parent.leftChild = NewNode;
+	  	        	  
+	  	              return; 
+	  	 
+	  	                    }
+	  	 
+	  	            } else { 
+	  	 
+	  	                    FocusNode = FocusNode.rightChild;
+	  	 
+	  	           if (FocusNode == null) {
+	  	 
+	  	                   parent.rightChild = NewNode;
+	  	                        return; // 
+	  	 
+	  	                    }
+	  	 
+	  	                }
+	  	 
+	  	            }
+	  	        }
+	  	 
+	  	    }
+	  	 
+	  	   
+	  	 
+	  	    public void inOrderTraverse(Node focusNode) {
+	  	 
+	  	        if (focusNode != null) {
+	  	 
+	  	           inOrderTraverse(focusNode.leftChild);
+	  	 
+	  	           System.out.println(focusNode);
+	  	 
+	  	           inOrderTraverse(focusNode.rightChild);
+	  	 
+	  	        }
+	  	 
+	  	    }
+	  	 
+	  	    public void preorderTraverse(Node focusNode) {
+	  	 
+	  	        if (focusNode != null) {
+	  	 
+	  	            System.out.println(focusNode);
+	  	 
+	  	            preorderTraverse(focusNode.leftChild);
+	  	            preorderTraverse(focusNode.rightChild);
+	  	 
+	  	        }
+	  	 
+	  	    }
+	  	 
+	  	    public void postOrderTraverse(Node focusNode) {
+	  	 
+	  	        if (focusNode != null) {
+	  	 
+	  	            postOrderTraverse(focusNode.leftChild);
+	  	            postOrderTraverse(focusNode.rightChild);
+	  	 
+	  	            System.out.println(focusNode);
+	  	 
+	  	        }
+	  	 
+	  	    }
+	  	 
+	  	    public Node findNode(int key) {
+	  
+	  	        Node focusNode = root;
+	  	 
+	  	     
+				while (focusNode.key != key) {
+				if (key < focusNode.key) {
+	  	 
+	  	            focusNode = focusNode.leftChild;
+	  	 
+	  	            } else {
+	  	 
+	  	               focusNode = focusNode.rightChild;
+	  	 
+	  	            }
+	  	 
+	  	          
+	  	 
+	  	            if (focusNode == null)
+	  	                return null;
+	  	 
+	  	        }
+	  	 
+	  	        return focusNode;
+	  	 
+	  	    }
+	  	 
+	  	public static void main(String[] args) {
+	  	 
+	  	        BinaryTree theTree = new BinaryTree();
+	  	 
+	  	        theTree.addNode(50,"A");
+	  	
+	  	        theTree.addNode(25, "B");
+	  	 
+	  	        theTree.addNode(75,"C");
+	  	 
+	  	        theTree.addNode(15,"D");
+	  	 
+	  	        theTree.addNode(30, "E");
+	  	 
+	  	        theTree.addNode(85,"F ");
+	  	 
+	  	         //theTree.inOrderTraverse(theTree.root);
+	  	 
+	  	         theTree.preorderTraverse(theTree.root);
+	  	 
+	  	         //theTree.postOrderTraverse(theTree.root);
+	  	 
+	  	        
+	  	 
+	  	      System.out.println("\nNode with the key ");
+	  	 
+	  	      System.out.println(theTree.findNode(25));
+	  	      System.out.println(theTree.root);
+	  	}
+	  	}
+	  	 
+	  class Node {
+	  	 
+	  	   int key;
+	  	 String name;
+	  	 
+	  	    Node leftChild;
+	  	    Node rightChild;
+	  	 
+	  	  Node(int key, String name){
+	  	 
+	  	       this.key = key;
+	  	       this.name = name;
+	  	 
+	  	    }
+	  	 
+	  	   public String toString() {
+	  	 
+	  	       return name + "  " ;
+	  	 
+	  	        /*
+	  	         * return name + " has the key " + key + "\nLeft Child: " + leftChild +
+	  	         * "\nRight Child: " + rightChild + "\n";
+	  	         */
+	  	 
+	  	   }
+	  	 
+	  }
+	 
+
